3ba0a15c8c6d3e6e44a2bd2b0dee68c38aa8ffb3
ArjunMalarmannan <arjun95srm@gmail.com>
2017-07-22 11:16:44 +0530

    oddreven.py

---
+++ oddreven.py
@@ -0,0 +1,11 @@
+x=int(input("Enter the number"))
+if(x%2)==0:
+	print("x is a even no.")
+else:
+	print("x is a odd no.")
+def oddreven(s):
+ if(s%2==0):
+  print("s is a even no.")
+ else:
+  print("s is a odd no.")
+ return s
